222bd7e8b306fd42f740fd0ba684619aada6e993
Sengolda <senushkabatawala@gmail.com>
2021-10-04 13:50:23 +0530

    .

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from fastapi.exceptions import HTTPException
 
 import random
 
-app = FastAPI(
-    debug=True
-)
+app = FastAPI(debug=True)
+
 
 def get_random_line(file):
     with open(file, "r", encoding="utf-8") as fp:
         lines = fp.read().splitlines()
         return random.choice(lines)
+
 
 @app.get("/", response_class=HTMLResponse)
 @app.get("/home", response_class=HTMLResponse)
@@ -23,21 +23,27 @@ async def hello():
 async def json():
     return RedirectResponse(url="/")
 
+
 @app.get("/api/fact", response_class=jsonify)
 async def get_fact():
     fact = get_random_line("storage/facts.txt")
-    if fact == "": # If it's an empty line
-        fact = get_random_line("storage/facts.txt") # Get a new fact.
-    
-    return jsonify({"text": fact, "fact": fact})
+    if fact == "":  # If it's an empty line
+        fact = get_random_line("storage/facts.txt")  # Get a new fact.
+
+    return jsonify(
+        {"text": fact, "fact": fact}
+        )
+
 
 @app.get("/api/website", response_class=jsonify)
 async def get_random_website():
     web_site = get_random_line("storage/websites.txt")
-    if web_site == "": # If it's an empty line
-        web_site = get_random_line("storage/facts.txt") # Get a new site.
+    if web_site == "":  # If it's an empty line
+        web_site = get_random_line("storage/facts.txt")  # Get a new site.
     
-    return jsonify({"text": web_site, "website": web_site, "site": web_site})
+    return jsonify(
+        {"text": web_site, "website": web_site, "site": web_site}
+    )
 
 
 @app.exception_handler(HTTPException)
