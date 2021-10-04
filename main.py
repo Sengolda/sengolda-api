b29016520dea5c06e6b6785b40d01ae3af6937a4
Sengolda <senushkabatawala@gmail.com>
2021-10-04 15:18:34 +0530

    API-keys and cats

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, JSONResponse as jsonify, RedirectResponse
+import random
+import uuid
+
+import pymongo
+from fastapi import FastAPI, Request, exceptions, status
+from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse as jsonify
+from fastapi.responses import RedirectResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-import random
+from config import mongo_db_url
 
+blacklitsed_agents = ["Bot", "Bots"]
 app = FastAPI(debug=True)
+mongo_cluster = pymongo.MongoClient(mongo_db_url)
+db = mongo_cluster["Sengolda-Api"]
 
 
 def get_random_line(file):
     with open(file, "r", encoding="utf-8") as fp:
         lines = fp.read().splitlines()
         return random.choice(lines)
+
+
+def check_request(request: Request):
+    key = request.headers.get("Authorization", None)
+    if not key:
+        return None
+    else:
+        col = db["api_keys"]
+        k = col.find({"key": key})
+        k = list(k)
+        try:
+            k_dict = k[0]
+        except Exception:
+            return None
+
+        if len(k_dict.keys()) == 0:
+            return False
+        else:
+            return True
+
+
+@app.get("/api/register")
+async def register_for_the_api(request: Request):
+    random_key = str(uuid.uuid4())
+    col = db["api_keys"]
+    col.insert_one({"key": random_key})
+    return jsonify({"sucess": True, "key": random_key})
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -25,25 +61,43 @@ async def json():
 
 
 @app.get("/api/fact", response_class=jsonify)
-async def get_fact():
+async def get_fact(request: Request):
+    check = check_request(request=request)
+    if not check:
+        return status.HTTP_401_UNAUTHORIZED
+
     fact = get_random_line("storage/facts.txt")
     if fact == "":  # If it's an empty line
         fact = get_random_line("storage/facts.txt")  # Get a new fact.
 
-    return jsonify(
-        {"text": fact, "fact": fact}
-        )
+    return jsonify({"text": fact, "fact": fact})
 
 
 @app.get("/api/website", response_class=jsonify)
-async def get_random_website():
+async def get_random_website(request: Request):
+    check = check_request(request=request)
+    if not check:
+        return status.HTTP_401_UNAUTHORIZED
+
     web_site = get_random_line("storage/websites.txt")
     if web_site == "":  # If it's an empty line
         web_site = get_random_line("storage/facts.txt")  # Get a new site.
-    
-    return jsonify(
-        {"text": web_site, "website": web_site, "site": web_site}
-    )
+
+    return jsonify({"text": web_site, "website": web_site, "site": web_site})
+
+
+@app.get("/api/cat", response_class=jsonify)
+@app.get("/api/cats", response_class=jsonify)
+async def get_random_cat(request: Request):
+    check = check_request(request=request)
+    if not check:
+        return status.HTTP_401_UNAUTHORIZED
+
+    url = get_random_line("storage/cats.txt")
+    if url == "":  # If it's an empty line
+        url = get_random_line("storage/cats.txt")  # Get a new image.
+
+    return jsonify({"file": url, "url": url})
 
 
 @app.exception_handler(StarletteHTTPException)
